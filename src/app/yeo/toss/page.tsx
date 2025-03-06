e7b60d448992ca1a83d5ea88a34bf253a2910886
soheeyeo <sohee407@gmail.com>
2025-03-06 17:01:20 +0900

    ✨ [Feat] Header 컴포넌트 구현

--- src/app/yeo/toss/page.tsx
+++ src/app/yeo/toss/page.tsx
@@ -1,11 +1,15 @@
+import Header from './components/header/Header'
 import HistoryList from './components/HistoryList'
 
 export default function TossPage() {
   return (
-    <section className="inset-auto bg-black">
-      <div className="min-h-[956px]">
-        <HistoryList />
-      </div>
-    </section>
+    <>
+      <Header />
+      <section className="inset-auto bg-black">
+        <div className="min-h-[956px]">
+          <HistoryList />
+        </div>
+      </section>
+    </>
   )
 }

f1c00aa985e4065db5736864d49c0ac85000bda9
soheeyeo <sohee407@gmail.com>
2025-03-24 18:34:00 +0900

    ✨ [Feat] VisionSection 컴포넌트 UI 및 스크롤 애니메이션 구현

--- src/app/yeo/toss/page.tsx
+++ src/app/yeo/toss/page.tsx
@@ -1,6 +1,7 @@
 import Header from './components/header/Header'
 import HistoryList from './components/historySection/HistoryList'
 import IntroList from './components/introSection/IntroList'
+import LogoSection from './components/visionSection/VisionSection'
 
 export default function TossPage() {
   return (
@@ -9,6 +10,7 @@ export default function TossPage() {
       <section className="inset-auto bg-black">
         <IntroList />
         {/* <HistoryList /> */}
+        <LogoSection />
       </section>
     </>
   )
